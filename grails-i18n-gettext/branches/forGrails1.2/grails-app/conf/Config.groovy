a045a3dbaadd665f858ef53f5beef7034eddf4eb
rabrab <rabrab@832c1c66-4827-0410-8465-ccb17913cd09>
2009-11-03 13:30:23 +0000

    Adaptions for grails 1.2

---
+++ grails-i18n-gettext/branches/forGrails1.2/grails-app/conf/Config.groovy
@@ -0,0 +1,8 @@
+t9n{
+	inputFileCharset = "UTF-8"
+    sourceCodeLocale = "en"
+}
+
+// The following properties have been added by the Upgrade process...
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
