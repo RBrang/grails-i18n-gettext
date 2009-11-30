66bff523d92c07fbdd285ea0c90ce9982b7e0b25
rabrab <rabrab@832c1c66-4827-0410-8465-ccb17913cd09>
2009-11-30 13:33:11 +0000

    Added more config options and fixed gant target handling

---
+++ grails-i18n-gettext/tags/LATEST_RELEASE/test/unit/T9nTagLibTests.groovy
@@ -0,0 +1,16 @@
+import grails.test.*
+
+class T9nTagLibTests extends GrailsUnitTestCase {
+	
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+    
+    void testSomething() {
+    }
+
+}
