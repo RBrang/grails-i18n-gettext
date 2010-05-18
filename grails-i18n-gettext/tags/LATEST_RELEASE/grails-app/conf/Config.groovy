dccbaaba2861657da3ab3ed24f1993610d9deb3e
rabrab <rabrab@832c1c66-4827-0410-8465-ccb17913cd09>
2010-05-18 20:12:46 +0000

    Updated for grails 1.3.1, especially fixed the gant scripts to run (again)

--- grails-i18n-gettext/tags/LATEST_RELEASE/grails-app/conf/Config.groovy
+++
@@ -1,8 +0,0 @@
-t9n{
-	inputFileCharset = "UTF-8"				// charset of all input files
-    sourceCodeLocale = "en"					// which is the original locale?
-    noWrapPoLines = false					// use -no-wrap parameter when creating/updating .po files
-
-    excludedDirsArray = ["scripts"]			// exclude these directories from scanning for translatable strings. 
-    										// (entries are relative to your project root)
-}
