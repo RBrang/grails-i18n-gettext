2eb72099e77c19b9ba6a28461c5a5115fcb2898e
Rainer Brang <info@backend-server.de>
2015-09-21 18:56:44 +0200

    Updated Grails to version 2.5.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,7 +12,7 @@ grails.project.dependency.resolution = {
 	// inherit Grails' default dependencies
 	inherits("global") {
 		// uncomment to disable ehcache
-		// excludes 'ehcache'
+		excludes 'ehcache'
 	}
 	log "error" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
 	checksums true // Whether to verify checksums on resolve
